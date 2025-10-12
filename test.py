3ef8d6b737de563746ab43900a0c17064f9da354
Sonali Chaudhari <choudharysonali713@gmail.com>
2025-10-12 22:10:45 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyB-QhYS03iyBN2AjYnfQxCaqkbR4CkvvAI")  # <-- put your API key here
-
-# Now list models
-models = list(genai.list_models())
-for m in models:
-    print(m)
